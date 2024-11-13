610beabaa3244ad0ae0d1a431145eaffd6cf1941
beaumontjonathan <jonathan@tuza.co.uk>
2024-11-13 09:55:13 +0000

    refactor(public-data-api): impove examples

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,13 +1,49 @@
+use minus::dynamic_paging;
 use std::env;
+use std::fmt::Write;
 
-use companies_house_api::{
-    operation::{
-        get_company_profile::GetCompanyProfile,
-        get_company_registered_office_address::GetCompanyRegisteredOfficeAddress,
-        search_companies::SearchCompanies, search_officers::SearchOfficers,
-    },
-    CompaniesHousePublicDataClient,
-};
+use companies_house_api::{operation, CompaniesHousePublicDataClient};
+use derive_more::derive::Display;
+
+#[derive(Debug, Display)]
+enum Operation {
+    #[display("Get company profile")]
+    GetCompanyProfile,
+    #[display("Get company registered office address")]
+    GetCompanyRegisteredOfficeAddress,
+    #[display("Search companies")]
+    SearchCompanies,
+    #[display("Search officers")]
+    SearchOfficers,
+    #[display("Search disqualified officers")]
+    SearchDisqualifiedOfficers,
+    Quit,
+}
+
+fn display_result<T: std::fmt::Debug, E: std::error::Error>(
+    result: Result<T, E>,
+) -> anyhow::Result<()> {
+    let mut pager = minus::Pager::new();
+    pager.set_exit_strategy(minus::ExitStrategy::PagerQuit)?;
+    match result {
+        Ok(inner) => writeln!(pager, "{inner:#?}")?,
+        Err(inner) => writeln!(pager, "{inner:#?}")?,
+    }
+    dynamic_paging(pager)?;
+    Ok(())
+}
+
+fn get_company_number() -> anyhow::Result<String> {
+    Ok(inquire::Text::new("Enter a company number:").prompt()?)
+}
+
+fn get_search_term() -> anyhow::Result<String> {
+    Ok(inquire::Text::new("Enter a search term:").prompt()?)
+}
+
+fn get_page_size() -> anyhow::Result<u8> {
+    Ok(inquire::CustomType::new("Enter a page size:").prompt()?)
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -15,42 +51,82 @@ async fn main() -> anyhow::Result<()> {
 
     let client = CompaniesHousePublicDataClient::new(&env::var("COMPANIES_HOUSE_API_KEY")?)?;
 
-    let data = client
-        .send(
-            GetCompanyRegisteredOfficeAddress::builder()
-                .company_number("00000006")
-                .build(),
+    loop {
+        let operation = inquire::Select::new(
+            "Choose an action:",
+            vec![
+                Operation::GetCompanyProfile,
+                Operation::GetCompanyRegisteredOfficeAddress,
+                Operation::SearchCompanies,
+                Operation::SearchOfficers,
+                Operation::SearchDisqualifiedOfficers,
+                Operation::Quit,
+            ],
         )
-        .await?;
+        .prompt()?;
 
-    println!("{data:#?}");
+        match operation {
+            Operation::GetCompanyProfile => {
+                let company_number = get_company_number()?;
+                let data = client
+                    .send(
+                        operation::get_company_profile::GetCompanyProfile::builder()
+                            .company_number(company_number)
+                            .build(),
+                    )
+                    .await;
+                display_result(data)?;
+            }
+            Operation::GetCompanyRegisteredOfficeAddress => {
+                let company_number = get_company_number()?;
+                let data = client
+                    .send(
+                        operation::get_company_registered_office_address::GetCompanyRegisteredOfficeAddress::builder()
+                            .company_number(company_number)
+                            .build(),
+                    )
+                    .await;
+                display_result(data)?;
+            }
+            Operation::SearchCompanies => {
+                let search_term = get_search_term()?;
+                let data = client
+                    .send(
+                        operation::search_companies::SearchCompanies::builder()
+                            .query(search_term)
+                            .build(),
+                    )
+                    .await;
+                display_result(data)?;
+            }
+            Operation::SearchOfficers => {
+                let search_term = get_search_term()?;
+                let data = client
+                    .send(
+                        operation::search_officers::SearchOfficers::builder()
+                            .query(search_term)
+                            .build(),
+                    )
+                    .await;
+                display_result(data)?;
+            }
+            Operation::SearchDisqualifiedOfficers => {
+                let search_term = get_search_term()?;
+                let items_per_page = get_page_size()?;
+                let data = client
+                .send(
+                    operation::search_disqualified_officers::SearchDisqualifiedOfficers::builder()
+                        .query(search_term)
+                        .items_per_page(Some(items_per_page as usize))
+                        .build(),
+                )
+                .await;
 
-    let data = client
-        .send(
-            GetCompanyProfile::builder()
-                .company_number("CE033707")
-                .build(),
-        )
-        .await?;
-
-    println!("{data:#?}");
-
-    let data = client
-        .send(SearchCompanies::builder().query("b").build())
-        .await?;
-
-    println!("{data:#?}");
-
-    let data = client
-        .send(
-            SearchOfficers::builder()
-                .query("b")
-                .items_per_page(Some(10))
-                .build(),
-        )
-        .await?;
-
-    println!("{data:#?}");
+                display_result(data)?;
+            }
+            Operation::Quit => break,
+        }
+    }
 
     Ok(())
 }
